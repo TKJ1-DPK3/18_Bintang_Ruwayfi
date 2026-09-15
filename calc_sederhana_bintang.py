0aebbcd39aa84d55a71f00fec1aa45cf0828e2dd
Bintang R18 <bintangruwayfi89@gmail.com>
2026-09-15 00:00:26 -0700

    Delete calc_sederhana_bintang.py

--- calc_sederhana_bintang.py
+++
@@ -1,8 +0,0 @@
-angka1=10
-angka2=3
-
-print("Penjumlahan:", angka1 + angka2)
-print("Pengurangan:", angka1 - angka2)
-print("Perkalian:", angka1 * angka2)
-print("Pembagian:", angka1 / angka2)
-print("Sisa Bagi:", angka1 % angka2)
