8ae00075ef5e213665176a19cd21afb919d44269
Bintang R18 <bintangruwayfi89@gmail.com>
2026-09-15 00:11:46 -0700

    Add files via upload

---
+++ Bintang Pertemuan 2 Perkenalan Diri/Bintang_Ruwayfi_As-Sulthan_perkenalan diri.py.py
@@ -0,0 +1,22 @@
+Nama=input("Masukkan Nama")
+Kelas=input("Masukkan Kelas")
+Umur=int(input("Masukkan Umur"))
+Jurusan=input("Masukkan Jurusan")
+Hobby=input("Masukkan Hobby Anda")
+print (
+
+
+
+
+)
+print("====MY PROFIL GW====")
+
+print(
+
+
+)
+print("Nama Saya     :", Nama)
+print("Saya Kelas    :", Kelas)
+print("Saya Umur     :", Umur, "Tahun")
+print("Jurusan Saya  :", Jurusan)
+print("Hobby Saya    :", Hobby)
