4604d81acde4a40e060d0728dd639a854d2af080
Bintang R18 <bintangruwayfi89@gmail.com>
2026-09-15 00:01:40 -0700

    Add files via upload

---
+++ Bintang Pertemuan 3 Kalkulator Sederhana/Bintang_kalkulatorsederhana.py.py
@@ -0,0 +1,8 @@
+angka1=10
+angka2=3
+
+print("Penjumlahan:", angka1 + angka2)
+print("Pengurangan:", angka1 - angka2)
+print("Perkalian:", angka1 * angka2)
+print("Pembagian:", angka1 / angka2)
+print("Sisa Bagi:", angka1 % angka2)
